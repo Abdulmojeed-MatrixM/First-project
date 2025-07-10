ed843b5ec3b65bd2226536bf5c7b9b0a83d83067
Abdulmojeed-MatrixM <aolayinkaolowu@gmail.com>
2025-07-10 20:03:38 +0100

    Xtra feautures on banking App

---
+++ Practical Activity/Simple Banking System/extraFeatures/index.js
@@ -0,0 +1,17 @@
+import BankAccount from './XbankAccount.js';
+
+// Example usage
+
+const account1 = new BankAccount(123456, 'AbduLLAh Muhammad', 1000, 'password123');
+const account2 = new BankAccount(789012, 'AbdurAbdurRahman Mahmoud', 500, 'password456');
+
+// Authenticate user
+account1.authenticate('password123'); // Successful authentication
+
+// Transfer funds
+account1.transferFunds(200, account2); // Transfer $200 from account1 to account2
+
+
+// Check balances
+account1.checkBalance(); // Check balance of account1
+account2.checkBalance(); // Check balance of account2
--- Practical Activity/Simple Banking System/index.js
+++ Practical Activity/Simple Banking System/index.js
@@ -5,12 +5,12 @@ const account1 = new BankAccount(123456, 'John Doe');
 const account2 = new BankAccount(789012, 'Jane Smith', 1000);
 
 // Deposit money
-account1.deposit(500);
-account2.deposit(200);
+account1.deposit(1500);
+account2.deposit(2000);
 
 // Withdraw money
-account1.withdraw(200);
-account2.withdraw(300);
+account1.withdraw(500);
+account2.withdraw(3000);
 
 // Check balances
 account1.checkBalance();
