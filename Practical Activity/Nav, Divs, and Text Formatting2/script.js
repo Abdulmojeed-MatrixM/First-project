2bea52935fcca5d2ee5e8f1c17d41e6f082e497b
Abdulmojeed-MatrixM <aolayinkaolowu@gmail.com>
2025-07-22 13:55:52 +0100

    manipulating classes - className, classList

---
+++ Practical Activity/Nav, Divs, and Text Formatting2/script.js
@@ -0,0 +1,17 @@
+//selecting the section with class "about-section"
+const section = document.querySelector(".about-section");
+
+//Adding an eventListener
+section.addEventListener("click", function() {
+    //Togggling the "about-section" class on the section
+    this.classList.toggle("about-section");
+   
+})
+
+//className is used to select the section with class "service-section"
+document.querySelector(".services-section").className;
+
+console.log("Service section class name:", document.querySelector(".services-section").className);
+
+//classList
+document.querySelector("nav").classList.add("header");
