9c3db1d2615fae6cd9aabf8f02c74a14bd6cbbe5
Abdulmojeed-MatrixM <aolayinkaolowu@gmail.com>
2025-08-07 20:27:57 +0100

    updated

--- JavaScript/DataTYPE/script.js
+++ JavaScript/DataTYPE/script.js
@@ -135,7 +135,7 @@ console.log("Standard Deviation:", stdDev);
 
 
 
-//Math Assignment:
+//Math Assignment: SELF
 let samples = [{"sample1": 4.2}, {"sample2": 5.1}, 
     {"sample3": 3.8}, {"sample4": 4.7}, {"sample5": 5.3}];
 //let sampleValues = Object.values(samples);
