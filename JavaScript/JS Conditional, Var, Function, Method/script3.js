37f2c902ba776305aaec72c0110aa4dfc7b85a38
Abdulmojeed-MatrixM <aolayinkaolowu@gmail.com>
2025-09-05 15:12:22 +0100

    update

--- JavaScript/JS Conditional, Var, Function, Method/script3.js
+++
@@ -1,6 +0,0 @@
-// Variable delclared as a String data type
-var firstName = "HasbunaLLAH";
-var lastName = "HamdaliLLAH";
-var fullName = firstName + " " + lastName;
-console.log("Here is my Full Name: " + fullName + ".");
-
--- M-project Activity/JS practise/Change Content Project/script.js
+++
@@ -1,6 +0,0 @@
-function changeHeadingText() {
-    // Target the id of the HTML heading element
-    var headingElement = document.getElementById("myHeading");
-    // Assign new text to the HTML heading element
-    headingElement.textContent = "New Heading Text";
-}
