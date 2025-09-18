6c7acfdead63b8840f875ed1e745c50612df821c
Abdulmojeed-MatrixM <aolayinkaolowu@gmail.com>
2025-09-18 18:07:50 +0100

    wroking on tailWindCSS

---
+++ ReactJSpractise/TailWindCSS/eslint.config.js
@@ -0,0 +1,34 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import { defineConfig, globalIgnores } from 'eslint/config'
+import tailwindcss from '@tailwindcss/vite' // Import Tailwind CSS plugin
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    plugins: [ // Add Tailwind CSS plugin
+      tailwindcss(),
+    ],
+    files: ['**/*.{js,jsx}'],
+    extends: [
+      js.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        ecmaFeatures: { jsx: true },
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+    },
+  },
+])
+
---
+++ ReactJSpractise/TailWindCSS/src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className="bg-amber-100 text-5xl font-bold underline"> PathBreakerM</h1>
+      <div>
+        
+      </div>
+      <h1 className="bg-yellow-500 p-7 rounded-2xl text-amber-900">PathBreakerM</h1>
+      
+      <p className="text-3xl font-bold underline">
+        Welcome to my TailWind CSS in React using VITE
+      </p>
+      
+    </>
+  )
+}
+
+export default App
