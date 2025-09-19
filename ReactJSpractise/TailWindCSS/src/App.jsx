4031cb9e9bf467f2a5ee176bfb8c4630fe295a7a
Abdulmojeed-MatrixM <aolayinkaolowu@gmail.com>
2025-09-19 16:19:41 +0100

    updated TailWindCSS

--- ReactJSpractise/TailWindCSS/src/App.jsx
+++ ReactJSpractise/TailWindCSS/src/App.jsx
@@ -1,4 +1,4 @@
-import './index.css'
+import './App.css'
 
 function App() {
 
@@ -19,6 +19,22 @@ function App() {
     </div>
 
     <h1 className="bg-yellow-700"> PathBreakerM </h1>
+
+    <div class="flex flex-col items-center gap-6 p-7 rounded-2xl">
+  <div>
+    <img class="size-48 shadow-xl rounded-md" alt="" src="/img/cover.png" />
+  </div>
+  <div class="flex items-center">
+    <span class="text-2xl font-medium">Class Warfare</span>
+    <span class="font-medium text-sky-500">The Anti-Patterns</span>
+    <span class="flex gap-2 font-medium text-gray-600 dark:text-gray-400">
+      <span>No. 4</span>
+      <span>·</span>
+      <span>2025</span>
+    </span>
+  </div>
+</div>
+
     </>
   )
 }
