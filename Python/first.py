d7332620cf5ef7ff794784a6d2ff2f9f3b8c8f41
Abdulmojeed-MatrixM <aolayinkaolowu@gmail.com>
2025-10-28 13:43:57 +0000

    updated data type conversion in Python

--- Python/first.py
+++ Python/first.py
@@ -5,4 +5,22 @@ if x<y:
     print("x is lesser than y")
 else:
     print("x is not equal to y")
-    #
+    
+    
+
+ #Data Type Conversion
+a=20
+b=25.9
+c=30j
+
+print(a)
+print(b)
+print(c)
+
+r=float(a)
+d=int(b)
+f=complex(a)
+
+print(r)
+print(d)
+print(f)
