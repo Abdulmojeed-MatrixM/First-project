5d1d9b6afe0822df0307f92a95a18b0df433893a
Abdulmojeed-MatrixM <aolayinkaolowu@gmail.com>
2025-10-28 15:05:57 +0000

    updated string format and random in python

--- Python/first.py
+++ Python/first.py
@@ -28,3 +28,23 @@ print(f)
 #Random Number generation
 import random
 print(random.randrange(1,20))
+
+#Loop through String
+x = "I love  Qur'an"
+for x in x:
+    print(x)
+
+#String Length
+x= "I love  Qur'an" 
+print(len(x))
+
+if "love" in x:
+    print("yes, Love is present")
+
+
+#String Format
+quantity = 5
+itemnumber = 45
+pricetag = 88.9
+myOrder = "I want to purchase {} pieces of items {} for {} pounds"
+print(myOrder.format(quantity, itemnumber, pricetag))
